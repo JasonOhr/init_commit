4c1a197d69bef5b7fc6527eddeec0f2e9df0cb39
Jason Ohr <jason.ohr@gmail.com>
2015-04-28 15:16:32 -0400

    added files to project

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 /**
  * Created by firewaterjoe on 4/27/15.
  */
-var hey = "dude";
-console.log(hey);
+var dude = document.getElementById("dude");
+var p = document.createElement("p");
+var node = document.createTextNode("Much Later on");
+p.appendChild(node);
+dude.appendChild(p);
+
+
