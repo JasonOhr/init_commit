1f8adbea267e781490159e7958746b56019eff8b
Jason Ohr <jason.ohr@gmail.com>
2015-04-29 09:50:57 -0400

    Added a README and .gitignore file as well as various other project files

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+/**
+ * Created by firewaterjoe on 4/28/15.
+ */
+module.exports = function(grunt){
+    //Configuration, Tasks, and plugins
+    grunt.initConfig({
+        pkg:grunt.file.readJSON('package.json')
+    });
+};
